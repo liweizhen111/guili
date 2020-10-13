5612a3da082c1eb28f0193d8131f0fe4e0b7a6e0
springcloudproject <springcloudproject@qq.com>
2020-10-13 23:20:17 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/liweizhen/guilimall/order/GuilimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liweizhen.guilimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/liweizhen/guilimall/guilimallware/GuilimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liweizhen.guilimall.guilimallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
